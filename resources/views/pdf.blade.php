d6bef3d5651be531005ea69fca14ca7440008bee
Erenesto Zhildeer Chura Flores <echura@craclasadev.com>
2022-11-21 12:22:50 -0500

    pdf view modificated with page break

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -82,8 +82,9 @@
                 @php
                     $temp++;
                 @endphp
+                <div style="page-break-after:always;"></div>
             @endforeach
-        </div><br><br><br>
+        </div>
     </main>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
